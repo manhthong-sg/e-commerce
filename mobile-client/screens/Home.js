131196bcea02f7365c53d633042b04962dfd7645
Manh Thong <manhthong1105@gmail.com>
2021-10-18 00:32:49 +0700

    fix bug cant connect server RN, done HomeHeader component

--- mobile-client/screens/Home.js
+++ mobile-client/screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Header from '../components/Home/Header'
+
 import HomeHeader from '../components/Home/HomeHeader'
 import SearchBar from '../components/Home/HomeHeader'
 import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper'
