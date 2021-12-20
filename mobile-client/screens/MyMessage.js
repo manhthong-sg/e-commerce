951000fb346a11a7f910180569ba5bd80d2eff80
Manh Thong <manhthong1105@gmail.com>
2021-12-20 14:26:17 +0700

    done 100% ui chat feature mobile side

--- mobile-client/screens/MyMessage.js
+++ mobile-client/screens/MyMessage.js
@@ -7,9 +7,10 @@ import { useSelector, useDispatch } from 'react-redux'
 const MyMessage = ({ navigation }) => {
     const CurrentUser = useSelector(state => state.userReducer.user);
 
+    const [message, setMessage] = useState("")
     const [messageData, setMessageData] = useState([
         {
-            userId: "CurrentUser._id",
+            userId: CurrentUser._id,
             roomId: "5720394857203948",
             message: "Xin chao cac ban",
             time: "12:23 AM, 17/12"
@@ -21,7 +22,7 @@ const MyMessage = ({ navigation }) => {
             time: "12:23 AM, 17/12"
         },
         {
-            userId: "CurrentUser._id",
+            userId: CurrentUser._id,
             roomId: "5720394857203948",
             message: "cho tui hoi xiu",
             time: "12:23 AM, 17/12"
@@ -79,76 +80,114 @@ const MyMessage = ({ navigation }) => {
                 width: "100%",
                 // backgroundColor: COLORS.cam1,
             }}>
-                {/* opposite side */}
-                <Text style={{
-                    fontSize: 10,
-                    color: COLORS.brand,
-                    marginLeft: 20,
-                }}>Supporter</Text>
-                <View
-                    style={{
-                        maxWidth: '70%',
-                        backgroundColor: COLORS.lightGray,
-                        // alignItems: "center",
-                        justifyContent: 'center',
-                        elevation: 0.2,
-                        height: 50,
-                        marginBottom: 5,
-                        borderTopRightRadius: 20,
-                        borderBottomRightRadius: 20,
-                        borderBottomLeftRadius: 20,
-                        elevation: 0.8,
-                        marginTop: 2,
-                        paddingLeft: 20,
-                        marginLeft: 20
-                    }}
-                >
-                    <Text style={{
-                        color: COLORS.black,
+                {
+                    (item.userId !== CurrentUser._id) ? (
+                        <View>
+                            {/* opposite side */}
+                            <Text style={{
+                                fontSize: 10,
+                                color: COLORS.brand,
+                                marginLeft: 20,
+                            }}>Supporter</Text>
+                            <View
+                                style={{
+                                    maxWidth: '70%',
+                                    backgroundColor: COLORS.lightGray,
+                                    // alignItems: "center",
+                                    justifyContent: 'center',
+                                    elevation: 0.2,
+                                    height: 50,
+                                    marginBottom: 5,
+                                    borderTopRightRadius: 10,
+                                    borderBottomRightRadius: 10,
+                                    borderBottomLeftRadius: 20,
+                                    elevation: 0.8,
+                                    marginTop: 2,
+                                    paddingLeft: 20,
+                                    marginLeft: 20,
+                                    marginBottom: 5,
+                                }}
+                            >
+                                <Text style={{
+                                    color: COLORS.black,
 
-                    }}>{item.message}</Text>
-                    <Text style={{
-                        fontSize: 8,
-                        color: COLORS.black,
-                        paddingTop: 5
-                    }}>{item.time}</Text>
-                </View>
-                {/* my side  */}
-                <View
-                    style={{
-                        maxWidth: '70%',
-                        backgroundColor: COLORS.brand,
-                        // alignItems: "center",
-                        justifyContent: 'center',
-                        elevation: 0.2,
-                        height: 50,
-                        marginBottom: 5,
-                        borderTopLeftRadius: 20,
-                        borderBottomLeftRadius: 20,
-                        borderBottomRightRadius: 20,
-                        elevation: 0.8,
-                        marginTop: 10,
-                        // paddingLeft: 20,
-                        paddingRight: 20,
-                        right: -100,
-                    }}
-                >
-                    <Text style={{
-                        color: COLORS.white,
-                        textAlign: 'right'
-                    }}>{item.message}</Text>
-                    <Text style={{
-                        fontSize: 8,
-                        color: COLORS.white,
-                        paddingTop: 5,
-                        textAlign: 'right'
-                    }}>{item.time}</Text>
-                </View>
-
+                                }}>{item.message}</Text>
+                                <Text style={{
+                                    fontSize: 8,
+                                    color: COLORS.black,
+                                    paddingTop: 5
+                                }}>{item.time}</Text>
+                            </View>
+                        </View>
+                    )
+                    :
+                    (
+                        // {/* my side  */}
+                        <View
+                            style={{
+                                maxWidth: '70%',
+                                backgroundColor: COLORS.brand,
+                                // alignItems: "center",
+                                justifyContent: 'center',
+                                elevation: 0.2,
+                                minHeight: 50,
+                                marginBottom: 5,
+                                borderTopLeftRadius: 10,
+                                borderBottomLeftRadius: 10,
+                                borderBottomRightRadius: 20,
+                                elevation: 0.8,
+                                marginTop: 10,
+                                // paddingLeft: 20,
+                                paddingRight: 10,
+                                paddingLeft: 15,
+                                marginBottom: 5,
+                                right: -100,
+                            }}
+                        >
+                            <Text style={{
+                                color: COLORS.white,
+                                textAlign: 'left'
+                            }}>{item.message}</Text>
+                            <Text style={{
+                                fontSize: 8,
+                                color: COLORS.white,
+                                paddingTop: 5,
+                                textAlign: 'right'
+                            }}>{item.time}</Text>
+                        </View>
+                    )
+                }
             </View>
         )
     }
 
+    //handle send message to ui
+    const handleSendMessage=()=>{
+        //convert format date to "3:12 PM, 23/11"
+        let today = new Date();
+        let hours=today.getHours();
+        let minutes=today.getMinutes();
+        let ampm = today.getHours() >= 12 ? 'PM' : 'AM';
+
+        if (hours > 12) {
+            hours=hours-12;
+        }
+        if(hours<10 ){
+            hours="0"+hours;
+        }
+        if(minutes<10){
+            minutes="0"+minutes;
+        }
+        let currentTime = hours + ":" + minutes + " " + ampm + ", " + today.getDate() + '/' + (today.getMonth() + 1)
+        
+        setMessageData([...messageData, {
+            userId: CurrentUser._id,
+            roomId: "5720394857203948",
+            message: message,
+            time: currentTime
+        }])
+        setMessage("");
+    }
     //main return
     return (
         <View style={styles.MyMessageContainer}>
@@ -160,6 +199,7 @@ const MyMessage = ({ navigation }) => {
                     paddingTop: 10,
                     height: "100%",
                     backgroundColor: COLORS.white,
+                    // paddingBottom: 10,
                     // maxHeight: 210,
                 }}
                 vertical
@@ -169,6 +209,39 @@ const MyMessage = ({ navigation }) => {
                 renderItem={renderMessage}
                 contentContainerStyle={{}}
             />
+            <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    backgroundColor: COLORS.white,
+                }}>
+                    <TextInput style={{
+                        height: 50,
+                        backgroundColor: COLORS.white,
+                        borderWidth: 0.5,
+                        // borderColor: COLORS.black,
+                        width: '80%',
+                        paddingLeft: 20,
+                        marginBottom: 60
+                    }}
+                        placeholder="Type here . ."
+                        onChangeText={msg=> setMessage(msg)}
+                        value={message}
+                    />
+                    <TouchableOpacity 
+                        style={{
+                            height: 50,
+                            width: 60,
+                            backgroundColor: COLORS.brand,
+                            marginLeft: 5,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            // marginTop: 20,
+                        }}
+                        onPress={handleSendMessage}
+                    >
+                        <Text style={{color: COLORS.primary}}>Send</Text>
+                    </TouchableOpacity>
+            </View>
         </View>
     )
 }
